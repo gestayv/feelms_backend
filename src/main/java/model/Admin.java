9a69d65992f2fa7dd6216fb84bc96ff8f03361d8
Arturo Salinas <arturo.salinas.dt@gmail.com>
2017-05-10 16:06:43 +0300

    Agregada una clase con facade y EJB, funciona en glassfish en servicio de prueba

--- src/main/java/model/Admin.java
+++ src/main/java/model/Admin.java
@@ -1,6 +1,7 @@
 package model;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 /**
  * Created by arturo on 10-05-17.
@@ -9,23 +10,51 @@ import javax.persistence.*;
 @Entity
 @Table(name="admins")
 //Ver si resulta sin esto
-@NamedQuery(name="Admin.findAll", query="SELECT a FROM admins a")
-public class Admin {
+@NamedQuery(name="Admin.findAll", query="SELECT a FROM Admin a")
+public class Admin implements Serializable {
 
     @Id
     @Column(name="id", unique=true, nullable=false)
     private int id;
 
-    @Column(name="username", unique=true, nullable=false)
+    @Column(name="username", unique=true, nullable=false, length=45)
     private String username;
 
-    @Column(name="password", nullable=false)
+    @Column(name="password", nullable=false, length=45)
     private String password;
 
-    @Column(name="first_name", nullable=false)
+    @Column(name="first_name", nullable=false, length=45)
     private String firstName;
 
-    @Column(name="last_name", nullable=false)
-    private String lastName;
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
--- src/main/java/service/TestService.java
+++ src/main/java/service/TestService.java
@@ -1,8 +1,14 @@
 package service;
 
+import facade.AdminFacade;
+import model.Admin;
+
+import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * Created by arturo on 02-05-17.
@@ -11,10 +17,23 @@ import javax.ws.rs.Produces;
 @Path("/test")
 public class TestService {
 
+    @EJB
+    AdminFacade adminFacadeEJB;
+
+    Logger logger = Logger.getLogger(TestService.class.getName());
+
     @GET
     @Produces("text/plain")
     public String getGreeting() {
         return "Hello Warudo!! :D";
     }
+
+    @GET
+    @Path("/admins")
+    @Produces({"application/xml", "application/json"})
+    public List<Admin> getAdmins() {
+        return adminFacadeEJB.findAll();
+    }
+
 
 }
